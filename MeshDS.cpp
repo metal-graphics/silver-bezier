822ed9bf72c30467a7de3ace93f98938d84bb1fb
Akash Raj <akashrajkn@gmail.com>
2014-11-21 12:42:58 +0530

    implemented mesh datastructure

---
+++ MeshDS.cpp
@@ -0,0 +1,86 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*   Copyright (c) 2014 Metal-Graphics                                        */
+/*   All rights reserved.                                                     */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+
+#include "MeshDS.h"
+
+char fileName[] = "one.off";
+/* MeshDS is parser for OFF format files */
+/* MeshDS takes input from OFF format files and returns a MeshDS object */
+
+MeshDS::MeshDS() //constructor
+{
+  
+}
+
+void MeshDS::initVertices( Point p ) //initializes vertices and forms a vector
+{
+  vertices.push_back(p);
+}
+
+//forms a data structure for vertices... a and b are vertex numbers(in vertices vector)
+void MeshDS::initEdges(int a, int b ) 
+{
+  edges.push_back(make_pair(a,b));
+}
+
+//it forms a ds for faces. vector size at each vector index denotes number of vertices for each face 
+void MeshDS::initFaces(vector<int> f)
+{
+  faces.push_back(f);
+}
+
+MeshDS MeshDS::meshObject()
+{
+  MeshDS obj = MeshDS();
+
+  ifstream input( fileName ); // off file input
+
+  if(input.is_open())
+  {
+    string format;
+    getline(input, format);
+
+    /*if(format.size()>3 || format[0]!='O' || format[1]!='F' || format[2]!='F')
+    {
+      cout<<"file is not in OFF format\n";
+      return NULL;
+    }
+    */
+
+    int numberVertices, numberEdges, numberFaces;
+
+    input>> numberVertices >> numberFaces >> numberEdges;
+
+    double tempx, tempy, tempz;
+    for(int i=0; i < numberVertices; ++i)
+    {
+      input>> tempx >> tempy >> tempz;
+      Point p = Point();
+      p.setxyz(tempx,tempy,tempz);
+      obj.initVertices(p);
+    }
+
+    int data, n; //n is teh number of vertices for each face, data is teh vertex number
+    for(int i=0; i< numberFaces; ++i)
+    {
+      input >> n;
+      vector<int> face; // size of teh this vector gives the number of vertices forthat face
+      for(int j=0; j<n; ++j)
+      {
+        input >> data;
+        face.push_back(data); //the vector face contains the vertex numbers reqd to make that face
+      }
+      obj.initFaces(face);
+    }
+
+    input.close();
+  }
+
+  return obj;
+}
+
---
+++ MeshDS.h
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*   Copyright (c) 2014 Metal-Graphics                                        */
+/*   All rights reserved.                                                     */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <utility>
+#include <fstream>
+#include "Point.cpp"
+
+using namespace std;
+
+class MeshDS
+{ 
+  public:
+    vector< Point > vertices;
+    vector< pair<int, int> > edges;
+    vector< vector<int> > faces;
+    MeshDS();
+    void initVertices(Point);
+    void initEdges(int, int);
+    void initFaces(vector<int>);
+    MeshDS meshObject();
+};
+
+
